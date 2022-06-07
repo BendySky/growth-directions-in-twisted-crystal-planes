a1cf1b93d5a4c72123a94d6add1772cfc67c1fa8
bessel-boi <57153033+fouriersFailure@users.noreply.github.com>
2022-06-07 00:45:36 -0400

    new function *mk_dataspace*; checks working folder for 3 existing directories ['raw_data','plots','unstitched_plots'] and creates new directories if they do not yet exist. also moves raw .txt files to 'raw_data' directory

--- datasets.py
+++ datasets.py
@@ -6,16 +6,47 @@ import pandas as pd
 import os, os.path
 import numpy as np
 import operator
+import shutil
+import glob
 
 
 class datasets:
 
-    def __init__(self, direc, direcList, direcNew, PeakAngle, IntOrAng):
+    def __init__(self, data_direc, direc, direcList, direcNew, maxVal, maxKey, IntOrAng):
         self.direc = direc
+        self.data_direc = data_direc
         self.direcList = direcList
         self.direcNew = direcNew
-        self.maxInt = PeakAngle
+        self.maxInt = maxInt
+        self.maxAngle = maxAngle
         self.IntOrAng = IntOrAng
+
+    def mk_dataspace(data_direc):
+
+        new_dirs = ['raw_data', 'plots', 'unstitched_plots']
+
+        # tells if the string 'raw_data' exists in the current directory
+        isdir = os.path.isdir(f'{data_direc}/{new_dirs[0]}')
+        if isdir == False:
+
+            print("Directories do not yet exist")
+            for items in new_dirs:
+                os.mkdir(items)
+
+                # merge current directory with new folder 'raw_data'
+            new_direc = os.path.join(data_direc, new_dirs[0])
+
+            pattern = '*.txt'
+            files = glob.glob(data_direc + pattern)
+
+            # moves text files to 'raw_data' folder
+            for file in files:
+                file_name = os.path.basename(file)
+                shutil.move(file, data_direc + new_dirs[0] + '/' + file_name)
+            print("\nInitial files moved to new filepath"
+                  "at:\n", data_direc + new_dirs[0])
+        else:
+            print("Directories already exist")
 
     # sorts the list of filenames by job number
     def sort_direcList(direcList, i):
