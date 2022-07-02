17a55d82e8d056300610a212dd2098ffce9cc8ba
bessel-boi <57153033+fouriersFailure@users.noreply.github.com>
2022-07-01 22:52:31 -0400

    Added quiver plots

--- colormaps.py
+++ colormaps.py
@@ -1,7 +1,7 @@
 import colorcet as cc
 
-def col_pal(color_code):
 
+def col_pal(color_code):
     col_scale = color_code
     arc_range = col_scale[0:64]
     col_scale.extend(arc_range)
@@ -9,14 +9,14 @@ def col_pal(color_code):
     return col_scale
 
 
-scale = {"rainbow1":cc.CET_C1s,
-         "rainbow2":cc.CET_R3,
-         "rainbow3":cc.CET_R4,
-         "rainbow4":cc.CET_C8s,
-         "workbow":cc.CET_C3,
-         "deuroto":cc.CET_CBC1,
-         "red_blue":cc.CET_C4s
-        }
+scale = {"rainbow1": cc.CET_C1s,
+         "rainbow2": cc.CET_R3,
+         "rainbow3": cc.CET_R4,
+         "rainbow4": cc.CET_C8s,
+         "workbow": cc.CET_C3,
+         "deuroto": cc.CET_CBC1,
+         "red_blue": cc.CET_C4s
+         }
 
 rainbow1 = col_pal(scale['rainbow1'])
 rainbow2 = col_pal(scale['rainbow2'])
@@ -28,7 +28,7 @@ red_blue = col_pal(scale['deuroto'])
 
 # Usage:
 
-#import colormaps as cm
-#in heatmap portion there is a parameter for the color scale called mapColor
-#example
-#ds.xrd_heatmap(**kwargs, ... , mapColor=cm.rainbow1)
+# import colormaps as cm
+# in heatmap portion there is a parameter for the color scale called mapColor
+# example
+# ds.xrd_heatmap(**kwargs, ... , mapColor=cm.rainbow1)
