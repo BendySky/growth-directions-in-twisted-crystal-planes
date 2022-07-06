13454e65437c993122e770d8d688109467982fa1
bessel-boi <57153033+fouriersFailure@users.noreply.github.com>
2022-07-06 18:15:34 -0400

    Refactored GUI

--- datasetsGUI.py
+++ datasetsGUI.py
@@ -1,254 +1,143 @@
-from mpl_toolkits.axes_grid1 import ImageGrid
-from matplotlib import pyplot as plt
-from math import log, pi
-from PIL import Image
-import seaborn as sns
-import pandas as pd
-import os, os.path
-import numpy as np
-import shutil
-import glob
+import tkinter as tk
+from tkinter import *
+import colorcet as cc
+from tkinter import filedialog as fd, LabelFrame
+from datasets import datasets as ds
 
+# from datasets import datasets as ds
+if __name__ == '__main__':
+    window = tk.Tk()
 
-class datasets:
+    window.wm_title("WAXS Growth Plane Analysis")
+    window.geometry("750x450+550+150")
+    #window.resizable(0, 0)
+    #window.attributes("-topmost", 1)
+    #window.attributes('-alpha', 1)
 
-    def __init__(self, direc, direcPath, data_direc, direcList, direcNew, PeakAngle, IntOrAng):
-        self.direc = direc
-        self.direcPath = direcPath
-        self.data_direc = data_direc
-        self.direcList = direcList
-        self.direcNew = direcNew
-        self.PeakAngle = PeakAngle
-        self.IntOrAng = IntOrAng
+    # noinspection PyTypeChecker
 
-    def ch_dir(direcPath):
+    def run_prog():
+        #is this ok???
+        direc_path = select_folder()
+        #???
+        sort_dir = ds.ch_dir(direc_path)
+        dfList = ds.df_to_list(sort_dir)
+        angleAtPeak = ds.getMaxPeak(dfList)
 
-        '''
-        :param direc: stores all filenames in a directory as a list
-        :param direcPath: folder path specified outside of class; describes location of data
-        '''
+        return angleAtPeak
 
-        os.chdir(f'{direcPath}')
-        datasets.mk_dataspace(direcPath)
-        os.chdir(f'{direcPath}/raw_data/')
-        direc = os.listdir(f'{direcPath}/raw_data')
-        sort_direc = datasets.imgOrPlot(direc)
+    def select_folder():
 
-        return sort_direc
+        direc_path = fd.askdirectory(title='Select Folder Containing Data', initialdir='/')
+        direc_path = f'{direc_path}/'
 
-    def mk_dataspace(data_direc):
+        return direc_path
 
-        '''
-        :param direcPath: folder path specified outside of class; describes location of data
-        '''
+    def file_select():
+        choice = file_var.get()
+        if choice == 1:
+            file = 'text'
+        elif choice == 2:
+            file = 'png'
+        elif choice == 3:
+            file = 'tiff'
+        return file
 
-        new_dirs = ['raw_data', 'plots', 'unstitched_plots']
+    def serp_sel():
+        choice=yn.get()
+        if choice == 1:
+             serp = True
+        elif choice == 2:
+            serp = False
+        return yn
 
-        # tells if the string 'raw_data' exists in the current directory
-        isdir = os.path.isdir(f'{data_direc}/{new_dirs[0]}')
-        if isdir == False:
+    #def run_prog():
 
-            print("Directories do not yet exist")
-            for items in new_dirs:
-                os.mkdir(items)
+    #    serp = serp_sel()
+    #    ds.xrd_heatmap(angleAtPeak, savepath=direc_path, IntOrAng=0, plt_size=row)
 
-                # merge current directory with new folder 'raw_data'
-            new_direc = os.path.join(data_direc, new_dirs[0])
+    file_var = IntVar()
+    yn = IntVar()
+    serp = StringVar()
+    color = StringVar()
+    qp_ti = StringVar()
+    qp_size = StringVar()
 
-            mime = os.listdir(data_direc)
-            if mime[1].find(".txt") != -1:
-                pattern = "*.txt"
-            elif mime[1].find(".png") != -1:
-                pattern = "*.png"
-            else:
-                pattern = "*.tiff"
+    filetype_options = tk.LabelFrame(window, text=' 2. File Type Options')
+    filetype_options.grid(row=0, columnspan=2, rowspan=3,
+                          sticky='W', padx=15, pady=5, ipady=5)
 
-            files = glob.glob(data_direc + pattern)
+    filetype = tk.LabelFrame(filetype_options, text='File Type')
+    filetype.grid(ipadx=15, ipady=4, padx=15, pady=0, row=1, column=0, columnspan=1, rowspan=3, sticky='S')
+    Radiobutton(filetype, text="Text", variable=file_var, value=1, command=file_select, pady=5).pack()
+    Radiobutton(filetype, text="PNG", variable=file_var, value=2, command=file_select, pady=5).pack()
+    Radiobutton(filetype, text="TIFF", variable=file_var, value=3, command=file_select, pady=5).pack()
 
-            # moves text files to 'raw_data' folder
-            for file in files:
-                file_name = os.path.basename(file)
-                shutil.move(file, data_direc + new_dirs[0] + '/' + file_name)
-            print("\nInitial files moved to new filepath"
-                  "at:\n", data_direc + new_dirs[0])
-        else:
-            print("Directories already exist")
+    plot_options = tk.LabelFrame(filetype_options, text='Plot Options')
+    plot_options.grid(ipadx=15, ipady=5, padx=15, pady=0, row=1, column=1, columnspan=1, rowspan=3)
 
-    # sorts the list of filenames by job number
-    def sort_direcList(direcList, i):
+    rows_label = Label(plot_options, text='Rows')
+    rows_label.grid(ipadx=4, padx=4, pady=1, row=0, column=1, sticky='W')
+    row_entry = Entry(plot_options)
+    row_entry.grid(ipadx=4, padx=4, pady=1, row=0, column=2, sticky='W')
 
-        """takes the created list and sorts the files by job number
-        direcList: path to directory stored as a list
-        i: column separation between '_' values"""
+    col_label = Label(plot_options, text='Columns')
+    col_label.grid(ipadx=4, padx=4, pady=5, row=1, column=1, sticky='W')
+    col_entry = Entry(plot_options)
+    col_entry.grid(ipadx=4, padx=4, pady=5, row=1, column=2, sticky='W')
 
-        direcList.sort(key=lambda x: int(x.split('_')[i]))
-        return direcList
+    serp_label = Label(plot_options, text='Serpentine')
+    serp_label.grid(ipadx=4, padx=4, pady=5, row=2, column=1, sticky='W')
+    serp_buttonY = Radiobutton(plot_options, text='Yes', variable=yn,
+                               value=1, pady=5).grid(row=2, column=2, sticky='W')
+    serp_buttonX = Radiobutton(plot_options, text='No', variable=yn,
+                               value=2, pady=5).grid(row=2, column=2, sticky='E')
 
-        # if-else statement is probably bad practice; is there a better way to edit this code??
+    plot_data = tk.LabelFrame(window, text=' 3. Plot Data')
+    plot_data.grid(row=8, columnspan=7, rowspan=5,
+                   sticky='NW', padx=15, pady=15, ipady=5)
+    heatmap_options = LabelFrame(plot_data, text='Heatmap Options')
+    heatmap_options.grid(padx=0, pady=0, row=2, column=0, columnspan=2)
 
-    def imgOrPlot(direc):
-        dir_new = []
-        # dir_new = set()
-        if direc[1].find('plot') != -1:
-            for i in direc:
-                dir_new = datasets.sort_direcList(direc, i=-4)
-                # newLab = i.replace('_waxs_stitched_plot.', '')
-                # dir_new.add(newLab)
+    hm_options = LabelFrame(plot_data, text='Heatmap Options')
+    hm_options.grid(row=0, column=0, padx=15, pady=15, ipady=5, sticky='NW')
 
-        else:
-            for i in direc:
-                dir_new = datasets.sort_direcList(direc, i=-3)
-                # newLab = i.replace('_waxs_stitched.tiff', '')
-                # dir_new.add(newLab)
-        # dir_new = list(dir_new)
-        return dir_new
+    plot_title = Label(hm_options, text='Plot Title')
+    plot_title.grid(padx=0, pady=5, row=0, column=0 , sticky='W')
+    plot_entry = Entry(hm_options)
+    plot_entry.grid(ipadx=4, padx=4, pady=5, row=0, column=1)
 
-    def df_to_list(direc):
+    plot_size = Label(hm_options, text='Plot Size')
+    plot_size.grid(pady=0, row=1, column=0, sticky='W')
+    size_entry = Entry(hm_options)
+    size_entry.grid(ipadx=4, padx=4, pady=0, row=1, column=1)
 
-        '''
-        :param direc: filenames stored in a list
-        :direcList: filenames converted into a nested dataframe.
-                    standard deviations are dropped as they are not needed
-        '''
+    color_scale = Label(hm_options, text='Color Scale')
+    color_scale.grid(padx=0, pady=5, row=2, column=0, sticky='W')
+    colors = ("Rainbow 1", "Rainbow 2", "Rainbow 3", "Deuroto", "Red-Blue")
+    color_entry = OptionMenu(hm_options, color, *colors)
+    color_entry.grid(padx=0, pady=5, row=2, column=1, sticky='W')
 
-        key = list(range(len(direc)))
-        col_list = [0, 1]
-        rename = ["Angle", "Intensity"]
-        direcList = []
+    qp_options = LabelFrame(plot_data, text='Quiver Plot Options')
+    qp_options.grid(row=0, column=1, padx=15, pady=15, ipady=5, sticky='NW')
 
-        for i in direc:
-            direcList.append(pd.read_csv(i, delimiter=' ', header=None, names=rename, usecols=col_list))
+    qp_title = Label(qp_options, text='Plot Title')
+    qp_title.grid(ipadx=4, padx=4, pady=5, row=0, column=1, sticky='W')
+    qp_title_entry = Entry(qp_options)
+    qp_title_entry.grid(ipadx=4, padx=4, pady=5, row=0, column=2, sticky='W')
 
-        # direcList = dict(zip(key, direcList))
+    qp_size = Label(qp_options, text='Plot Size')
+    qp_size.grid(ipadx=4, padx=4, pady=5, row=1, column=1, sticky='W')
+    qp_size_entry = Entry(qp_options)
+    qp_size_entry.grid(ipadx=4, padx=4, pady=5, row=1, column=2, sticky='W')
+    trans = Label(qp_options, text='Transparent')
+    trans.grid(ipadx=4, padx=4, pady=5, row=2, column=1, sticky='W')
+    #Radiobutton(qp_options, text='Yes', row=2, column=2).pack()
 
-        return direcList
+    direc_button = Button(window, text='Browse ...', command=select_folder)
+    direc_button.grid(padx=15, row=20, column=0, sticky='W')
 
-    def logInt(PeakAngle):
+    #run_button = Button(window, text=' Run ', command=run_prog)
+    #run_button.grid(padx=10, row=20, column=0)
 
-        # calculates the log10 value of the intensity
-        # and returns it in the same list
-        for i in range(len(PeakAngle)):
-            PeakAngle[i][1] = log(PeakAngle[i][1])
-
-        return PeakAngle
-
-    # def getMaxPeak(dataFn, PeakAngle):
-    def getMaxPeak(dataFn):
-
-        PeakAngle = []
-
-        # map max intensity to corresponding angle
-        for i in range(len(dataFn)):
-            PeakAngle.append(dataFn[i].loc[dataFn[i]['Intensity'].idxmax()].tolist())
-
-        # use logInt method to convert intensity to log(intensity)
-        datasets.logInt(PeakAngle)
-        return PeakAngle
-
-    def contour_plot(PeakAngle, savepath='', plot_title='', plot_size=15., trans=True):
-
-        mesh = []
-        size = int(np.sqrt(len(PeakAngle)))
-
-        # convert array into meshgrid and re-arrange order so
-        # the quiver plot matches the micrograph, heatmaps etc.
-        for i in range(len(PeakAngle)):
-            mesh.append(PeakAngle[i][0])
-
-        mesh.reverse()
-        mesh = np.meshgrid(mesh)
-        mesh = np.reshape(mesh, (size, size))
-
-        for i in range(len(mesh)):
-            mesh[i] = mesh[i][::-1]
-
-        fig, ax = plt.subplots(figsize=(plot_size, plot_size))
-
-        # create empty list for coordinates of quiver plot
-        arr = []
-        range(int(np.sqrt(len(PeakAngle))))
-        for i in range(int(np.sqrt(len(PeakAngle)))):
-            arr.append(i)
-
-        x_pos, y_pos = arr, arr
-
-        # map angles to corresponding locations on sinosoidal plot.
-        # see the trigonometric circle to get a better understanding
-        # of what is happening here
-        X, Y = np.cos(mesh * (pi / 180)), np.sin(mesh * (pi / 180))
-
-        # quiver plot
-        ax.quiver(x_pos, y_pos, X, Y, pivot='middle')
-        ax.title.set_text(plot_title)
-        plt.savefig(f'{savepath}/plots/quiver_plot.png', transparent=trans)
-        plt.show()
-
-    def xrd_heatmap(PeakAngle, savepath='', IntOrAng=1, plt_size=1.0, plotTitle='', mapColor=''):
-
-        reshape = []
-        size = int(np.sqrt(len(PeakAngle)))
-
-        for i in range(len(PeakAngle)):
-            reshape.append(PeakAngle[i][IntOrAng])
-
-        mapDim = np.reshape(reshape, (size, size))
-
-        plt.figure(figsize=(plt_size, plt_size))
-        plt.title(plotTitle, fontdict={'fontsize': '35'}, pad='10')
-        ax = sns.heatmap(mapDim, cmap=mapColor, annot=True, fmt='0g', square=True)
-        plt.savefig(f'{savepath}/plots/{plotTitle}.png')
-        plt.show()
-
-    def angleVintensity_plots(direcList, dir_name='current_dataset'):
-
-        # foldername = os.path.basename(dir_name)
-        # path = os.path.join(savepath, foldername)
-        # os.mkdir(path)
-
-        for i in range(len(direcList)):
-            plt.title(f'XRD_{i}');
-            direcList[i].plot(x='Angle', y='Intensity', title=f'XRD_{i}');
-            plt.savefig(f'{dir_name}/unstitched_plots/1d_plots_{i}_.png');
-
-    def plot_scans(filePath, rows_cols=(1, 1), serpentine=False, filetype='text'):
-
-        if filetype == 'text':
-            grab_path = f'{filePath}/unstitched_plots'
-            sort = -2
-        elif filetype == 'tiff':
-            grab_path = f'{filePath}/raw_data'
-            sort = -3
-        elif filetype == 'png':
-            grab_path = f'{filePath}/raw_data'
-            sort = -3
-
-        direc = os.listdir(grab_path)
-
-        fs = int(np.sqrt(len(direc)))
-
-        direc = datasets.sort_direcList(direc, i=sort)
-        direc = np.reshape(direc, rows_cols)
-        if serpentine:
-            print("serpentine scans were used; adjusting accordingly")
-            direc[1::2, :] = direc[1::2, ::-1]
-        else:
-            print("No serpentine scans. plot order unchanged")
-        direc = np.concatenate(direc)
-        direc = direc.tolist()
-
-        imgs = []
-        valid_images = ['.png', '.tiff']
-
-        for i in direc:
-            ext = os.path.splitext(i)[1]
-            if ext.lower() not in valid_images:
-                continue
-            imgs.append(Image.open(os.path.join(grab_path, i)))  # filePath
-
-        fig = plt.figure(figsize=(145., 145.))
-        grid = ImageGrid(fig, 111, nrows_ncols=rows_cols, direction='row')
-
-        for ax, im in zip(grid, imgs):
-            ax.imshow(im)
-
-        plt.savefig(f'{filePath}/plots/stitched_plot.png')
+    window.mainloop()
