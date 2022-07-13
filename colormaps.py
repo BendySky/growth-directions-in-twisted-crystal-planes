8cfd114076351e0ad9bd7bcf234d8967c77937c5
BendySky <57153033+BendySky@users.noreply.github.com>
2022-07-13 12:27:02 -0400

    Extended color scale range

--- colormaps.py
+++ colormaps.py
@@ -3,13 +3,13 @@ import colorcet as cc
 
 def col_pal(color_code):
     col_scale = color_code
-    arc_range = col_scale[0:64]
+    arc_range = col_scale[0:128]
     col_scale.extend(arc_range)
 
     return col_scale
 
 
-scale = {"rainbow1": cc.CET_C1s,
+trig_scale = {"rainbow1": cc.CET_C1s,
          "rainbow2": cc.CET_R3,
          "rainbow3": cc.CET_R4,
          "rainbow4": cc.CET_C8s,
@@ -18,13 +18,13 @@ scale = {"rainbow1": cc.CET_C1s,
          "red_blue": cc.CET_C4s
          }
 
-rainbow1 = col_pal(scale['rainbow1'])
-rainbow2 = col_pal(scale['rainbow2'])
-rainbow3 = col_pal(scale['rainbow3'])
-rainbow4 = col_pal(scale['rainbow4'])
-workbow = col_pal(scale['workbow'])
-deuroto = col_pal(scale['deuroto'])
-red_blue = col_pal(scale['red_blue'])
+rainbow1 = col_pal(trig_scale['rainbow1'])
+rainbow2 = col_pal(trig_scale['rainbow2'])
+rainbow3 = col_pal(trig_scale['rainbow3'])
+rainbow4 = col_pal(trig_scale['rainbow4'])
+workbow = col_pal(trig_scale['workbow'])
+deuroto = col_pal(trig_scale['deuroto'])
+red_blue = col_pal(trig_scale['red_blue'])
 
 # Usage:
 
